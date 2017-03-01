3c6464a3454c9d4c212fe382c1d709fa8fddcfbc
JordiGonzalez7 <gonzlezsanchezjordi@gmail.com>
2017-03-01 17:24:11 +0100

    en esta actualizacion hemos añadido el boton de la camara, dentro de este ya funciona correctamente el boton 1 y 2

--- app/src/main/java/com/example/jordi/practicafinal2jordijose/Camara.java
+++ app/src/main/java/com/example/jordi/practicafinal2jordijose/Camara.java
@@ -1,8 +1,8 @@
 package com.example.jordi.practicafinal2jordijose;
 
-import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -11,17 +11,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import java.io.File;
 
 public class Camara extends AppCompatActivity {
 
-    private Button btn1, btn2, btn3;
+    private Button btn1c, btn2c, btn3c;
     private ImageView img1;
-    public final static int APP_CAMERA = 1;
+    private int tipo;
+    private Bitmap bMap;
+
 
     private Uri idIMG;
 
@@ -31,13 +31,55 @@ public class Camara extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camara);
 
-        btn1 = (Button) findViewById(R.id.btn1);
-        btn2 = (Button) findViewById(R.id.btn2);
-        btn3 = (Button) findViewById(R.id.btn3);
+        btn1c = (Button) findViewById(R.id.btn1);
+        btn2c = (Button) findViewById(R.id.btn2);
+        btn3c = (Button) findViewById(R.id.btn3);
         img1 = (ImageView) findViewById(R.id.img1);
 
 
-        btn3.setOnClickListener(new View.OnClickListener() {
+        btn1c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE_SECURE);
+
+                File fotodir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "FotosPrueba");
+                fotodir.mkdirs();
+
+                File foto = new File(fotodir, "foto1.jpg");
+                Uri uriSavedImage = Uri.fromFile(foto);
+
+                i.putExtra(MediaStore.EXTRA_OUTPUT, uriSavedImage);
+
+                idIMG = Uri.fromFile(foto);
+                tipo = 1;
+                startActivityForResult(i, tipo);
+            }
+        });
+
+
+        btn2c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE_SECURE);
+
+                File fotodir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "FotosPrueba");
+                fotodir.mkdirs();
+
+                File foto = new File(fotodir, "foto1.jpg");
+                Uri uriSavedImage = Uri.fromFile(foto);
+
+                i.putExtra(MediaStore.EXTRA_OUTPUT, uriSavedImage);
+
+                idIMG = Uri.fromFile(foto);
+                tipo = 2;
+                startActivityForResult(i, tipo);
+
+            }
+        });
+
+
+        btn3c.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -50,45 +92,34 @@ public class Camara extends AppCompatActivity {
     }
 
 
-    public void foto(View v) {
-        Intent i = new Intent("android.media.action.IMAGE_CAPTURE");
-
-        File foto = new File(getExternalFilesDir(null), "fotos.jpg");
-        i.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(foto));
-
-        idIMG = Uri.fromFile(foto);
-        startActivityForResult(i, APP_CAMERA);
-
-    }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         Log.e("requestCode", "requestCode:" + requestCode);
         Log.e("RESULT_OK", "RESULT_OK:" + RESULT_OK);
 
-        switch (requestCode) {
 
-            case APP_CAMERA:
-                if (resultCode == RESULT_OK) {
+        if (requestCode == 1 && resultCode == RESULT_OK) {
 
+            switch (tipo) {
 
-                    ContentResolver cr = getContentResolver();
-                    cr.notifyChange(idIMG, null);
-                    Bitmap bitmap;
+                case 1:
+                    bMap = BitmapFactory.decodeFile(
+                            getExternalFilesDir(Environment.DIRECTORY_PICTURES) +
+                                    "/FotosPrueba/" + "foto1.jpg");
 
-                    try {
-                        bitmap = android.provider.MediaStore.Images.Media.getBitmap(cr, idIMG);
-                        int alt = (int) (bitmap.getHeight() * 1080 / bitmap.getWidth());
-                        Bitmap reduit = Bitmap.createScaledBitmap(bitmap, 1080, alt, true);
-                        img1.setImageBitmap(reduit);
+                    img1.setImageBitmap(bMap);
+                    break;
+                case 2:
+                    bMap = BitmapFactory.decodeFile(
+                            getExternalFilesDir(Environment.DIRECTORY_PICTURES) +
+                                    "/FotosPrueba/" + "foto1.jpg");
 
-                    } catch (Exception e) {
-                        Toast.makeText(getApplicationContext(), "No se ha cargado la imagen " + idIMG.toString(), Toast.LENGTH_LONG).show();
+                    img1.setImageBitmap(bMap);
+                    img1.getLayoutParams().height = 77;
+                    img1.getLayoutParams().width = 55;
+                    break;
 
-                    }
-                }
-
-                break;
+            }
         }
 
     }
